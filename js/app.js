128982c26f0cd7be845afdf969af982535824516
mahimur <hellomahimur@gmail.com>
2022-09-03 01:29:43 +0600

    creating  news category link done

--- js/app.js
+++ js/app.js
@@ -1,18 +1,32 @@
 fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(res => res.json())
-    .then(data => loadNewsdata(data.data.news_category))
+    .then(data => loadNewsCategories(data.data.news_category))
     .catch(error => console.log(error));
 
-const loadNewsdata = newsCategories => {
+const loadNewsCategories = newsCategories => {
     const newsCategoriesContainer = document.getElementById('news-categories-container');
     newsCategories.forEach(newsCategory => {
         // console.log(newsCategory.category_name);
         const newsCategoryLi = document.createElement('li');
         newsCategoryLi.classList.add('nav-item');
         newsCategoryLi.innerHTML = `
-          <a class="nav-link active mx-4 fs-5" aria-current="page"
+          <a onclick="displayNewsInCategory(${newsCategory.category_id})" class="nav-link active mx-4 fs-5" id="category" aria-current="page"
            href="#">${newsCategory.category_name}</a>
         `;
         newsCategoriesContainer.appendChild(newsCategoryLi);
     });
+};
+
+const displayNewsInCategory = async newsId => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${newsId}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    loadNewsCategories(data.data);
 }
+
+// const displayNewsInCategory = category => {
+//     const url = ` https://openapi.programming-hero.com/api/news/category/${category_id}`;
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     console.log(data.data);
+// }
